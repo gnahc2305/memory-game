88718914e29ea521e999d9ef4bf73548592cd013
gnahc2305 <achangmurillo@gmail.com>
2022-11-11 10:38:19 -0500

    score now works

--- src/components/Content.js
+++ src/components/Content.js
@@ -22,33 +22,34 @@ const Content = () => {
   function onClick(e) {
     let numberClicked = parseInt(e.target.textContent);
     // save numver clicked on the numberSelected array
-    // numbersSelected.push(numberClicked);
     setNumbers(numbersSelected.concat(numberClicked));
 
     // delete numbers and generete a new set
     numbers = [];
     createRandomNumbers()
-    // console.log(numbers)
 
     setVisible((prev) => !prev);
   }
 
-  // press p to test things
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "p") {
-      console.log(numbersSelected);
-      // console.log(numbers);
-    }
-  });
+  // test button
+  function check() {
+    console.log(numbersSelected)
+  }
+
 
   return (
     // display every number
-    <div className="content">
-      {numbers.map((number) => (
-        <div className="card" key={uniqid()} onClick={onClick}>
-          {number}
-        </div>
-      ))}
+    <div style={{textAlign: "center"}}>
+      <h3>Score: {numbersSelected.length}</h3>
+      <button onClick={check}>Test</button>
+
+      <div className="content">
+        {numbers.map((number) => (
+          <div className="card" key={uniqid()} onClick={onClick}>
+            {number}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
